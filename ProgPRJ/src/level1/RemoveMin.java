7fe5925e378f4a1aac8d9a31758e9a4b5e328fb6
dupyo <ghdrnvy0922@naver.com>
2020-09-24 17:19:45 +0900

    completed RemoveMin 2020-09-24 17:19

---
+++ ProgPRJ/src/level1/RemoveMin.java
@@ -0,0 +1,34 @@
+package level1;
+
+import java.util.Arrays;
+
+public class RemoveMin {
+
+	public static void main(String[] args) {
+		//제일 작은 수 제거하기
+		/*int [] arr = {10,11,4,7};
+		System.out.println(Arrays.toString(new Level1().solution2(arr)));*/
+	}
+	//제일 작은 수 제거하기
+	public int[] solution2(int[] arr) {
+		int i = 0,j = 0;
+		if(arr.length == 1) {
+			arr[0]=-1;
+			return arr;
+		}
+		int [] ar2 = arr.clone();
+		Arrays.sort(ar2);
+		int min = ar2[0];
+
+		int[] answer = new int [arr.length-1];
+		while(j<answer.length) {
+			if(min != arr[i]) {
+				answer[j]=arr[i];
+				j++;
+			}
+			i++;
+		}
+		return answer;
+	}
+
+}
