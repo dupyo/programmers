b71340e3a6b8462ab1872ae4479840c032930a18
dupyo <ghdrnvy0922@naver.com>
2020-10-13 09:48:35 +0900

    corrected level1/GCDLCM 2020-10-13 09:48

--- ProgPRJ/src/level1/GCDLCM.java
+++ ProgPRJ/src/level1/GCDLCM.java
@@ -7,12 +7,12 @@ public class GCDLCM {
 	public static void main(String[] args) {
 		//최대공약수와 최소공배수
 		int n = 42, m = 12;
-		System.out.println(Arrays.toString(new GCDLCM().solution(n, m)));
+		System.out.println(Arrays.toString(new GCDLCM().solution2(n, m)));
 	}
 	
 	//최대공약수와 최소공배수
 	public int[] solution(int n, int m) {
-		if(n > m) {
+		if(n > m) { //n은 m보다 작거나 같도록 한다.
 			int sw=n;
 			n=m;
 			m=sw;
@@ -25,6 +25,21 @@ public class GCDLCM {
 			answer[1]++;
 		
 		return answer;
-	} //유클리드 호제법 응용
+	}
+	
+	public int[] solution2(int n, int m) { //유클리드 호제법 응용 - 두 수를 곱한 뒤 최대공약수를 나누면 최소공배수
+		int [] answer = new int [2];
+		if(n > m) { //n은 m보다 작거나 같도록 한다.
+			int sw=n;
+			n=m;
+			m=sw;
+		}
+		answer[0]=n;
+		while(!(n%answer[0] == 0 && m%answer[0] == 0)) //최대공약수 구하는 방법
+			answer[0]--;
+		answer[1]=n*m/answer[0]; //최소공배수 구하는 방법
+		return answer;
+		
+	}
 
 }
