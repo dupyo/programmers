1c2cc4b69f52a0c998bc7a85a866c470ac149f49
dupyo <ghdrnvy0922@naver.com>
2020-10-14 11:22:43 +0900

    completed level1/IntervalDigits 2020-10-14 11:22

---
+++ ProgPRJ/src/level1/IntervalDigits.java
@@ -0,0 +1,20 @@
+package level1;
+
+import java.util.Arrays;
+
+public class IntervalDigits {
+
+	public static void main(String[] args) {
+		//x만큼 간격이 있는 n개의 숫자
+		int x=19, n=19;
+		System.out.println(Arrays.toString(new IntervalDigits().solution(x, n)));
+	}
+	//x만큼 간격이 있는 n개의 숫자
+	public long[] solution(int x, int n){
+		long[] answer = new long [n];
+		for(int i=0; i < n; i++)
+			answer[i]=(long)x*(i+1);
+		return answer;
+	}
+
+}
