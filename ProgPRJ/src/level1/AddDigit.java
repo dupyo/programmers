67aba7654ac353a93e984d8f1f77585811e29dfd
dupyo <ghdrnvy0922@naver.com>
2020-10-16 10:13:36 +0900

    corrected level1/AddDigit 2020-10-16 10:13

--- ProgPRJ/src/level1/AddDigit.java
+++ ProgPRJ/src/level1/AddDigit.java
@@ -4,25 +4,28 @@ public class AddDigit {
 
 	public static void main(String[] args) {
 		//자릿수 더하기
-		int n = 987;
+		int n = 983;
 		System.out.println(new AddDigit().solution(n));
 	}
 	public int solution(int n) {
 		int answer = 0;
-		int dig = 1;
-		int add = 0;
+		/*int dig = 1;
 		for(int i=1; i<=100000000; i*=10) {
 			if(n/i < 10)
 				break;
 			else
 				dig++;
-		}
-		//int dig = Integer.toString(n).length();
+		}*/
+		int dig = Integer.toString(n).length();
+		/*int add = 0;
 		while(dig > 0) {
 			add = (int)Math.pow(10, --dig);
 			answer += (n/add);
 			n-=(n/add)*add;
-		}
+		}*/
+		char[] nArr = Integer.toString(n).toCharArray();
+		for(int i=0; i < nArr.length; i++)
+			answer+=Character.getNumericValue(nArr[i]);
 		return answer;
 	}
 
