404e66bbd20e382d20ca1fbe89b91ce8a991291b
dupyo <ghdrnvy0922@naver.com>
2021-03-22 18:28:51 +0900

    2021-03-22 18:27 coding test 210320 Task2

--- ProgPRJ/src/codingTest_210320/Task2.java
+++ ProgPRJ/src/codingTest_210320/Task2.java
@@ -3,8 +3,41 @@ package codingTest_210320;
 public class Task2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(new Task2().solution(4, "1111"));
+	}
+	
+	public int solution(int a, String b) {
+		int answer = 0;
+		
+		while(b.contains("101"))
+			b=b.replaceAll("101", "1");
+		
+		int std_o = b.length()-1;
+		int std_ot = b.length()-1;
+		int ot = 1;
+		int one = 1;
+		int two = 1;
 
+		while(std_o>=1) {
+			//첫번째 수와 두번째 수의 합의 팩토리얼
+			for(int i=std_ot; i >= 1; i--)
+				ot*=i;
+			//첫번째 수의 팩토리얼
+			for(int i=std_o; i >= 1; i--)
+				one*=i;
+			//두번째 수의 팩토리얼
+			for(int i=std_ot-std_o; i >= 1; i--)
+				two*=i;
+			
+			std_o-=2;
+			std_ot--;
+			answer+=ot/(one*two);
+			ot=1;
+			one=1;
+			two=1;
+		}
+		
+		return answer;
 	}
 
 }
