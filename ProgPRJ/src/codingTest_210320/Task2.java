f15556420a9b182fefb2338cdba9e7937113363b
dupyo <ghdrnvy0922@naver.com>
2021-03-23 17:58:22 +0900

    2021-03-23 17:58 coding test 210320 Task2

--- ProgPRJ/src/codingTest_210320/Task2.java
+++ ProgPRJ/src/codingTest_210320/Task2.java
@@ -3,7 +3,7 @@ package codingTest_210320;
 public class Task2 {
 
 	public static void main(String[] args) {
-		System.out.println(new Task2().solution(4, "1111"));
+		System.out.println(new Task2().solution(4, "11"));
 	}
 	
 	public int solution(int a, String b) {
@@ -17,8 +17,9 @@ public class Task2 {
 		int ot = 1;
 		int one = 1;
 		int two = 1;
-
-		while(std_o>=1) {
+		
+		//같은 것이 있는 순열 구하기
+		while(std_o>=0) {
 			//첫번째 수와 두번째 수의 합의 팩토리얼
 			for(int i=std_ot; i >= 1; i--)
 				ot*=i;
@@ -29,8 +30,8 @@ public class Task2 {
 			for(int i=std_ot-std_o; i >= 1; i--)
 				two*=i;
 			
-			std_o-=2;
 			std_ot--;
+			std_o-=2;
 			answer+=ot/(one*two);
 			ot=1;
 			one=1;
