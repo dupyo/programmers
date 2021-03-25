79f8913e74f2e5d13b7d998fb22882662aa8c5f2
dupyo <ghdrnvy0922@naver.com>
2021-03-25 10:46:15 +0900

    2021-03-25 coding test 210312 Task3

--- ProgPRJ/src/codingTest_210312/Task3.java
+++ ProgPRJ/src/codingTest_210312/Task3.java
@@ -3,22 +3,23 @@ package codingTest_210312;
 public class Task3 {
 
 	public static void main(String[] args) {
-		System.out.println("" + new Task3().solution("CABCD"));
+		System.out.println("" + new Task3().solution("ABACBADCCDC"));
 	}
 	
 public String solution(String S) {
 		String N [] = {"AB", "BA", "CD", "DC"};
-		int cnt = 0;
 		String pre = "";
+		int cnt = 0;
 		int is_over_4 = 0;
+
 		while(is_over_4 < 5) {
-			pre=S;
-			S=S.replaceAll(N[cnt%4], "");
+			S=S.replaceAll(N[cnt++%4], "");
 			if(pre.equals(S))
 				is_over_4++;
-			else
+			else {
+				pre=S;
 				is_over_4=0;
-			cnt++;
+			}
 		}
 		
 		return S;
