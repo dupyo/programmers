9a1ad1be5ff5d370fd8b482ec74c8d8f89347d0b
dupyo <ghdrnvy0922@naver.com>
2021-03-29 18:15:37 +0900

    2021-03-29 Competition1 corrected

--- ProgPRJ/src/competitions/Competition1.java
+++ ProgPRJ/src/competitions/Competition1.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Competition1 {
 
 	public static void main(String[] args) {
-		int [] numbers = {5,0,2,7};
+		int [] numbers = {5,0,2,7,1,3,2,7,4};
 		System.out.println(Arrays.toString(new Competition1().solution(numbers)));
 	}
 	
@@ -17,13 +17,14 @@ public class Competition1 {
 	public int[] solution(int[] numbers) {
 		int sum = 0;
         List<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(numbers);
         for(int i=0; i<numbers.length; i++) {
         	for(int j=i+1; j<numbers.length; j++) {
         		if(!list.contains(sum=numbers[i]+numbers[j]))
         			list.add(sum);
         	}
         }
-        Collections.sort(list);
+        
         int[] answer = new int [list.size()];
         for(int i=0; i<list.size(); i++)
         	answer[i]=list.get(i);
