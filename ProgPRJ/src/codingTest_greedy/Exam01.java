5eaa79cdd774a5cd3ed33e198a4b49ba5c29eeae
dupyo <ghdrnvy0922@naver.com>
2021-04-06 14:14:46 +0900

    2021-04-06 greedy exam01 completed

---
+++ ProgPRJ/src/codingTest_greedy/Exam01.java
@@ -0,0 +1,22 @@
+package codingTest_greedy;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		int N=25, k=3;
+		System.out.println(new Exam01().solution(N, k));
+	}
+	
+	public int solution(int N, int k) {
+		int answer=0;
+		while(N>1) {
+			if(N%k == 0)
+				N=N/k;
+			else
+				N--;
+			answer++;
+		}
+		return answer;
+	}
+
+}
