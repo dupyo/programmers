a0ee1bdea67e3e5b98f1c74f9d0658f699904ada
dupyo <ghdrnvy0922@naver.com>
2021-04-06 14:16:51 +0900

    2021-04-06 14:16 greedy exam01 corrected

--- ProgPRJ/src/codingTest_greedy/Exam01.java
+++ ProgPRJ/src/codingTest_greedy/Exam01.java
@@ -10,10 +10,11 @@ public class Exam01 {
 	public int solution(int N, int k) {
 		int answer=0;
 		while(N>1) {
-			if(N%k == 0)
+			/*if(N%k == 0)
 				N=N/k;
 			else
-				N--;
+				N--;*/
+			N=(N%k==0) ? N/k : N-1;
 			answer++;
 		}
 		return answer;
