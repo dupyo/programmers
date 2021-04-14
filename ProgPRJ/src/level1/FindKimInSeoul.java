9c10a66fc31c26f9d6fc1b37ff6baa2639e45ca1
dupyo <ghdrnvy0922@naver.com>
2021-04-14 10:50:54 +0900

    2021-04-14 level1/FindKimInSeoul completed

---
+++ ProgPRJ/src/level1/FindKimInSeoul.java
@@ -0,0 +1,23 @@
+package level1;
+
+import java.util.Arrays;
+
+public class FindKimInSeoul {
+
+	public static void main(String[] args) {
+		String[] seoul = {"Jane", "Kia", "Srr", "Kim", "Srr"};
+		System.out.println(new FindKimInSeoul().solution(seoul));
+	}
+	
+	public String solution(String[] seoul) {
+		String answer="";
+//		int i = Arrays.asList(seoul).indexOf("Kim");
+//		answer="김서방은 "+i+"에 있다";
+		int i=0;
+		while(!(seoul[i].equals("Kim")))
+			i++;
+		answer="김서방은 "+i+"에 있다";
+		return answer;
+	}
+
+}
