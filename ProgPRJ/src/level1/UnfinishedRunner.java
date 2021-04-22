33b676b932401a2d6852703489c174e1e1c4c8d3
dupyo <ghdrnvy0922@naver.com>
2021-04-22 11:11:55 +0900

    2021-04-22 level1/UnfinishedRunner comment

--- ProgPRJ/src/level1/UnfinishedRunner.java
+++ ProgPRJ/src/level1/UnfinishedRunner.java
@@ -17,8 +17,13 @@ public class UnfinishedRunner {
 	public String solution(String[] participant, String[] completion) {
 		String answer = "";
 		HashMap<String, Integer> runner = new HashMap<>();
-		for(String n : participant) runner.put(n, runner.getOrDefault(n, 0)+1);
-		for(String n : completion) runner.put(n, runner.get(n)-1);
+		//key,value 형식으로 참가자 저장
+		for(String n : participant)
+			runner.put(n, runner.getOrDefault(n, 0)+1);
+		//key,value 형식으로 완주자 제거
+		for(String n : completion)
+			runner.put(n, runner.get(n)-1);
+		//완주하지 못한 참가자의 value는 0이 아님
 		for(String n : runner.keySet()) {
 			if(runner.get(n) != 0) {	//완주하지 못한 참가자
 				answer=n;
