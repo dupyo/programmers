db36963bd32fd2e0610e172b7e7ab9395e1a884c
dupyo <ghdrnvy0922@naver.com>
2021-04-23 10:25:43 +0900

    2021-04-23 level1/PnYInString completed

---
+++ ProgPRJ/src/level1/PnYInString.java
@@ -0,0 +1,23 @@
+package level1;
+
+public class PnYInString {
+
+	public static void main(String[] args) {
+		//문자열 내 p와 y의 개수
+		String s="pPoooyY";
+		System.out.println(new PnYInString().solution(s));
+	}
+	//문자열 내 p와 y의 개수
+	public boolean solution(String s) {
+		boolean answer=false;
+		int cnt=0;
+		s=s.toLowerCase();
+		//문자열에서 p의 개수와 y의 개수의 차를 이용
+		cnt=s.length()-s.replaceAll("p", "").length();
+		cnt-=s.length()-s.replaceAll("y", "").length();
+		if(cnt == 0)
+			answer=true;
+		return answer;
+	}
+
+}
