ea768181e715dd31b2e524295d38700179b04eb6
dupyo <ghdrnvy0922@naver.com>
2021-05-18 14:23:23 +0900

    2021-05-18 level1/CaesarCipher completed

---
+++ ProgPRJ/src/level1/CaesarCipher.java
@@ -0,0 +1,25 @@
+package level1;
+
+public class CaesarCipher {
+
+	public static void main(String[] args) {
+		//시저 암호
+		String s = "a B z Z";
+		int n = 4;
+		System.out.println(new CaesarCipher().solution(s, n));
+	}
+	public String solution(String s, int n) {
+		String answer = "";
+		for(int i = 0; i < s.length(); i++) {
+			char factor = s.charAt(i);
+			if(factor == ' ')
+				answer+=' ';
+			else if(factor + n > 'z' || (factor + n > 'Z' && factor <= 'Z'))
+				answer+=(char)(factor+n-26);
+			else
+				answer+=(char)(factor+n);
+		}
+		return answer;
+	}
+
+}
