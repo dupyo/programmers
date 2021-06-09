487d1fe17132528c34daa4bcd47b693298af2e78
user <user@DESKTOP-39TB3AV>
2021-06-09 12:57:15 +0900

    2021-06-09 patternMatching/RemoveUnmatchedCode completed

--- ProgPRJ/src/patternMatching/RemoveUnmatchedCode.java
+++ ProgPRJ/src/patternMatching/RemoveUnmatchedCode.java
@@ -1,16 +1,16 @@
-package kim;
+package patternMatching;
 
-public class KimPass {
+public class RemoveUnmatchedCode {
 
 	public static void main(String[] args) {
 //		String p = "110";
 		String p = "101";
 //		String p = "00";
 //		String s = "110110110110";
-		String s = "10100010101101100";
+		String s = "101000101011011001";
 //		String s = "100000001";
 //		String s = "10111111";
-		System.out.println(new KimPass().solution(p, s));
+		System.out.println(new RemoveUnmatchedCode().solution(p, s));
 	}
 	
 	public int solution(String p, String s) {
@@ -22,23 +22,19 @@ public class KimPass {
 			if(p.charAt(pIdx%p.length()) == s.charAt(i)) {
 				tmp+=p.charAt(pIdx%p.length());
 				pIdx++;
-				System.out.println("i : " + i+ " tmp : " + tmp);
 				if(tmp.equals(p)) {
 					tmp="";
 				}
 			} else {
 				answer++;
-				System.out.println("i : " + i+" answer : " + answer);
 				res+=s.charAt(i);
 			}
 		}
-		System.out.println("------");
 		if(tmp.length() > 0 && !tmp.equals(p)) {
 			answer+=tmp.length();
 			res+=tmp;
-			System.out.println("tmp length : " + tmp.length());
 		}
-		System.out.println("res : "+ res);
+		System.out.println(res);
 		if(s.length() == res.length())
 			return -1;
 		else
