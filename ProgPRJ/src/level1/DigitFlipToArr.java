78c89fa592d29959ce63731d172a140b4086ee9d
dupyo <ghdrnvy0922@naver.com>
2021-07-13 19:47:59 +0900

    입력값의 각 요소를 char로 파싱한 후 int로 형 변환 해서 배열에 저장

--- ProgPRJ/src/level1/DigitFlipToArr.java
+++ ProgPRJ/src/level1/DigitFlipToArr.java
@@ -5,30 +5,17 @@ import java.util.Arrays;
 public class DigitFlipToArr {
 
 	public static void main(String[] args) {
-		//자연수 뒤집어 배열로 만들기
+		// 자연수 뒤집어 배열로 만들기
 		long n = 999898;
-		System.out.println(Arrays.toString(new DigitFlipToArr().solution(n)));
+		System.out.println(Arrays.toString(solution(n)));
 	}
-	
-	//자연수 뒤집어 배열로 만들기
-	public int[] solution(long n) {
-		/*int dig = 1;
-		long n2 = n;
-		while(true) {
-			n2/=10;
-			if(n2+9 < 10)
-				break;
-			dig++;
-		}
-		int[] answer = new int [dig];
-		for(int i=0; i < dig; i++) {
-			answer[i]=(int)(n%10);
-			n/=10;
-		}*/
+
+	// 자연수 뒤집어 배열로 만들기
+	public static int[] solution(long n) {
 		String nStr = Long.toString(n);
-		int [] answer = new int [nStr.length()];
-		for(int i=0; i < nStr.length(); i++)
-			answer[i]=Integer.parseInt(""+nStr.charAt(nStr.length()-1-i));
+		int[] answer = new int[nStr.length()];
+		for (int i = 0; i < nStr.length(); i++)
+			answer[i] = Integer.parseInt("" + nStr.charAt(nStr.length() - 1 - i));
 		return answer;
 	}
 
