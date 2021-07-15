809fae64f1201f5017c587d1856a8ead456b4e93
dupyo <ghdrnvy0922@naver.com>
2021-07-15 15:00:13 +0900

    코드 정리

--- ProgPRJ/src/level1/GCDLCM.java
+++ ProgPRJ/src/level1/GCDLCM.java
@@ -5,41 +5,37 @@ import java.util.Arrays;
 public class GCDLCM {
 
 	public static void main(String[] args) {
-		//최대공약수와 최소공배수
+		// 최대공약수와 최소공배수
 		int n = 42, m = 12;
-		System.out.println(Arrays.toString(new GCDLCM().solution2(n, m)));
+		System.out.println(Arrays.toString(solution2(n, m)));
 	}
-	
-	//최대공약수와 최소공배수
-	public int[] solution(int n, int m) {
-		if(n > m) { //n은 m보다 작거나 같도록 한다.
-			int sw=n;
-			n=m;
-			m=sw;
+
+	// 최대공약수와 최소공배수
+	public static int[] solution(int n, int m) {
+		if (n > m) { // n은 m보다 작거나 같도록 한다.
+			int sw = n;
+			n = m;
+			m = sw;
 		}
-		int [] answer = {n, m}; //최종 결과 : answer[0](최대공약수) <= answer[1](최소공배수)
-		while(!(n%answer[0] == 0 && m%answer[0] == 0)) //최대공약수 구하는 방법
-			answer[0]--;
-		
-		while(!(answer[1]%m == 0 && answer[1]%n == 0)) //최소공배수 구하는 방법
-			answer[1]++;
-		
+		int[] answer = { n, m }; // 최종 결과 : answer[0](최대공약수) <= answer[1](최소공배수)
+		while (!(n % answer[0] == 0 && m % answer[0] == 0)) answer[0]--; // 최대공약수 구하는 방법
+		while (!(answer[1] % m == 0 && answer[1] % n == 0)) answer[1]++; // 최소공배수 구하는 방법
+
 		return answer;
 	}
-	
-	public int[] solution2(int n, int m) { //유클리드 호제법 응용 - 두 수를 곱한 뒤 최대공약수를 나누면 최소공배수
-		int [] answer = new int [2];
-		if(n > m) { //n은 m보다 작거나 같도록 한다.
-			int sw=n;
-			n=m;
-			m=sw;
+
+	public static int[] solution2(int n, int m) { // 유클리드 호제법 응용 - 두 수를 곱한 뒤 최대공약수를 나누면 최소공배수
+		int[] answer = new int[2];
+		if (n > m) { // n은 m보다 작거나 같도록 한다.
+			int sw = n;
+			n = m;
+			m = sw;
 		}
-		answer[0]=n;
-		while(!(n%answer[0] == 0 && m%answer[0] == 0)) //최대공약수 구하는 방법
-			answer[0]--;
-		answer[1]=n*m/answer[0]; //최소공배수 구하는 방법
+		answer[0] = n;
+		while (!(n % answer[0] == 0 && m % answer[0] == 0)) answer[0]--; // 최대공약수 구하는 방법
+		answer[1] = n * m / answer[0]; // 최소공배수 구하는 방법
 		return answer;
-		
+
 	}
 
 }
--- ProgPRJ/src/level1/IntervalDigits.java
+++ ProgPRJ/src/level1/IntervalDigits.java
@@ -5,15 +5,16 @@ import java.util.Arrays;
 public class IntervalDigits {
 
 	public static void main(String[] args) {
-		//x만큼 간격이 있는 n개의 숫자
-		int x=19, n=19;
-		System.out.println(Arrays.toString(new IntervalDigits().solution(x, n)));
+		// x만큼 간격이 있는 n개의 숫자
+		int x = 19, n = 19;
+		System.out.println(Arrays.toString(solution(x, n)));
 	}
-	//x만큼 간격이 있는 n개의 숫자
-	public long[] solution(int x, int n){
-		long[] answer = new long [n];
-		for(int i=0; i < n; i++)
-			answer[i]=(long)x*(i+1);
+
+	// x만큼 간격이 있는 n개의 숫자
+	public static long[] solution(int x, int n) {
+		long[] answer = new long[n];
+		for (int i = 0; i < n; i++)
+			answer[i] = (long) x * (i + 1);
 		return answer;
 	}
 
