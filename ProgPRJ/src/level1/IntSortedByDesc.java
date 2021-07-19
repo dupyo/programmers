b9bb2babc63ab738fc9f5a919b2f002f8b914b72
dupyo <ghdrnvy0922@naver.com>
2021-07-19 20:02:57 +0900

    코드 정리

--- ProgPRJ/src/level1/IntSortedByDesc.java
+++ ProgPRJ/src/level1/IntSortedByDesc.java
@@ -3,12 +3,12 @@ package level1;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class IntToDesc {
+public class IntSortedByDesc {
 
 	public static void main(String[] args) {
 		//정수 내림차순으로 배치하기
 		long n = 1231392567;
-		System.out.println(Long.toString(new IntToDesc().solution2(n)));
+		System.out.println(Long.toString(new IntSortedByDesc().solution2(n)));
 	}
 	//정수 내림차순으로 배치하기
 	public long solution2(long n) {
