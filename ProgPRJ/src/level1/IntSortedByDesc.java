26ae94bf7fca9c5c19d47c0cf559a24780fd57ae
dupyo <ghdrnvy0922@naver.com>
2021-07-21 21:29:32 +0900

    코드 정리

--- ProgPRJ/src/level1/IntSortedByDesc.java
+++ ProgPRJ/src/level1/IntSortedByDesc.java
@@ -6,13 +6,14 @@ import java.util.Collections;
 public class IntSortedByDesc {
 
 	public static void main(String[] args) {
-		//정수 내림차순으로 배치하기
+		// 정수 내림차순으로 배치하기
 		long n = 1231392567;
 		System.out.println(Long.toString(new IntSortedByDesc().solution2(n)));
 	}
-	//정수 내림차순으로 배치하기
+
+	// 정수 내림차순으로 배치하기
 	public long solution2(long n) {
-		String [] nStrs = Long.toString(n).split("");
+		String[] nStrs = Long.toString(n).split("");
 		Arrays.sort(nStrs, Collections.reverseOrder());
 		String nStr = String.join("", nStrs);
 		return Long.parseLong(nStr);
