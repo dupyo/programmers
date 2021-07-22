1ec257902d75dff82b6f11fc3c0bb60107a68ad5
dupyo <ghdrnvy0922@naver.com>
2021-07-22 20:55:52 +0900

    else 예약어 쓰지 않기. 두 점수를 비교하는 메소드 분리.

--- ProgPRJ/src/level1/MockTest.java
+++ ProgPRJ/src/level1/MockTest.java
@@ -1,53 +1,48 @@
 package level1;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class MockTest {
 
 	public static void main(String[] args) {
 		// 모의고사
-		int[] answers = { 3, 3, 2, 4, 5, 4, 4, 2, 4 };
-		System.out.println(Arrays.toString(new MockTest().solution(answers)));
+		int[] answers = { 1, 3, 2, 4, 2 };
+		System.out.println(Arrays.toString(solution(answers)));
 	}
-
+	
+	// 두 점수를 비교하여 더 큰 값을 반환
+	public static int compare(int a, int b) {
+		return Math.max(a, b);
+	}
+	
 	// 모의고사
-	public int[] solution(int[] answers) {
-		int[] sps = new int[3];
-		List<Integer> spMax = new ArrayList<>();
-		int[][] sp123 = { { 1, 2, 3, 4, 5 }, { 2, 1, 2, 3, 2, 4, 2, 5 }, { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 } };
+	public static int[] solution(int[] answers) {
+		int[] scoreboard = new int[3];
+		int winnerSize = 0;
+		int[][] supojas = { { 1, 2, 3, 4, 5 }, { 2, 1, 2, 3, 2, 4, 2, 5 }, { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 } };
+		int max = 0;
+		// 리스트를 만들어서 배열로 바꾸는 방법은 되도록 쓰지 말자.
+		// 생성할 배열의 길이를 구하고 배열을 생성.
 
-		for (int i = 0; i < sps.length; i++) {
+		for (int i = 0; i < scoreboard.length; i++) {
 			for (int j = 0; j < answers.length; j++) {
-				if (sp123[i][(j % sp123[i].length)] == answers[j]) {
-					sps[i]++;
-				}
+				if (supojas[i][(j % supojas[i].length)] == answers[j])
+					scoreboard[i]++;
 			}
+			max = compare(max, scoreboard[i]);
 		}
-		for (int i = 0; i < sps.length; i++) {
-			if (sps[i] != 0) {
-				for (int j = 0; j < sps.length; j++) {
-					if (sps[j] != 0) {
-						if (sps[i] > sps[j]) {
-							sps[j] = 0;
-						} else if (sps[i] < sps[j]) {
-							sps[i] = 0;
-							break;
-						}
-					}
-				}
-			}
+		
+		for (int i = 0; i < scoreboard.length; i++) {
+			if (scoreboard[i] == max)
+				winnerSize++;
 		}
-		for (int i = 0; i < sps.length; i++) {
-			if (sps[i] != 0) {
-				spMax.add(i + 1);
-			}
+		
+		int [] answer = new int [winnerSize];
+		for(int i = 0; i < scoreboard.length; i++) {
+			if(scoreboard[i] == max)
+				answer[answer.length-(winnerSize--)] = i + 1;
 		}
-		int[] answer = new int[spMax.size()];
-		for (int i = 0; i < spMax.size(); i++) {
-			answer[i] = spMax.get(i);
-		}
+		
 		return answer;
 	}
 }
