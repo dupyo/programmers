c6b5620a39cb9f45e46af9af8a3b89794f4ab1d3
dupyo <ghdrnvy0922@naver.com>
2021-07-22 21:13:55 +0900

    조건에 맞으면 break 제어문 쓰지 않고 바로 리턴하기

--- ProgPRJ/src/level1/UnfinishedRunner.java
+++ ProgPRJ/src/level1/UnfinishedRunner.java
@@ -1,36 +1,30 @@
 package level1;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 
 public class UnfinishedRunner {
 
 	public static void main(String[] args) {
-		String[] participant = {"mislav", "stanko", "mislav", "ana"};
-		String[] completion = {"stanko", "ana", "mislav"};
-		System.out.println(new UnfinishedRunner().solution(participant, completion));
+		// 완주하지 못한 선수
+		String[] participant = { "marina", "josipa", "nikola", "vinko", "filipa" };
+		String[] completion = { "josipa", "filipa", "marina", "nikola" };
+		System.out.println(solution(participant, completion));
 
 	}
-	
-	public String solution(String[] participant, String[] completion) {
-		String answer = "";
+
+	public static String solution(String[] participant, String[] completion) {
 		HashMap<String, Integer> runner = new HashMap<>();
-		//key,value 형식으로 참가자 저장
-		for(String n : participant)
-			runner.put(n, runner.getOrDefault(n, 0)+1);
-		//key,value 형식으로 완주자 제거
-		for(String n : completion)
-			runner.put(n, runner.get(n)-1);
-		//완주하지 못한 참가자의 value는 0이 아님
-		for(String n : runner.keySet()) {
-			if(runner.get(n) != 0) {	//완주하지 못한 참가자
-				answer=n;
-				break;
-			}
+		// key,value 형식으로 참가자 저장
+		for (String n : participant)
+			runner.put(n, runner.getOrDefault(n, 0) + 1);
+		// key,value 형식으로 완주자 제거
+		for (String n : completion)
+			runner.put(n, runner.get(n) - 1);
+		// 완주하지 못한 참가자의 value는 0이 아님
+		for (String n : runner.keySet()) {
+			if (runner.get(n) != 0) // 완주하지 못한 참가자
+				return n;
 		}
-		
-		return answer;
+		return "";
 	}
 }
