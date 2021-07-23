d4cc0f0aa4853b85033c35981b340ad068d39f6b
dupyo <ghdrnvy0922@naver.com>
2021-07-23 22:36:33 +0900

    코드 정리 필요함

--- ProgPRJ/src/level1/NoSameNum.java
+++ ProgPRJ/src/level1/NoSameNum.java
@@ -7,27 +7,28 @@ import java.util.List;
 public class NoSameNum {
 
 	public static void main(String[] args) {
-		int[] arr = {1, 1, 3, 3, 0, 1, 1};
-		System.out.println(Arrays.toString(new NoSameNum().solution(arr)));
+		// 같은 숫자는 싫어
+		int[] arr = { 1, 1, 3, 3, 0, 1, 1 };
+		System.out.println(Arrays.toString(solution(arr)));
 	}
-	
-	public int[] solution(int []arr) {
-        int pre = arr[0];
+
+	public static int[] solution(int[] arr) {
+		int pre = arr[0];
 		List arrList = new ArrayList();
 		arrList.add(arr[0]);
-		
-		for(int i=1; i < arr.length; i++) {
-			if(pre!=arr[i]) {
+
+		for (int i = 1; i < arr.length; i++) {
+			if (pre != arr[i]) {
 				arrList.add(arr[i]);
-				pre=arr[i];
+				pre = arr[i];
 			}
 		}
-		
-		int [] answer = new int [arrList.size()];
-		for(int i=0; i < arrList.size(); i++)
-			answer[i]=(int)arrList.get(i);
-		
+
+		int[] answer = new int[arrList.size()];
+		for (int i = 0; i < arrList.size(); i++)
+			answer[i] = (int) arrList.get(i);
+
 		return answer;
-    }
+	}
 
 }
