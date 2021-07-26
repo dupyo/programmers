9090297ea24b2596236edf8c9972bdb9ebdc4363
dupyo <ghdrnvy0922@naver.com>
2021-07-26 17:08:49 +0900

    리스트 타입 쓰지 않고 배열로 문제 해결하기

--- ProgPRJ/src/level1/NoSameNumber.java
+++ ProgPRJ/src/level1/NoSameNumber.java
@@ -6,14 +6,16 @@ public class NoSameNumber {
 
 	public static void main(String[] args) {
 		// 같은 숫자는 싫어
+		// 미션 1. else 예약어 쓰지 않기
+		// 미션 2. 리스트 타입 쓰지 않기
 		int[] arr = { 1, 1, 3, 3, 0, 1, 1 };
 		System.out.println(Arrays.toString(solution(arr)));
 	}
 
 	public static int[] solution(int[] arr) {
 
-		// 제거된 후 남은 수를 저장할 배열을 생성하기 위해 중복 없는 배열의 길이를 구하라
-		// 길이를 구하면 반복문에서 임시로 이전 값을 저장해서 중복을 허용하지 않도록 조건을 걸어라
+		// 중복 없는 배열을 생성하기 위해 중복 없는 배열의 길이를 먼저 구하라
+		// 길이를 구하면 입력된 배열에서 이전 값을 기억하여 중복 없는 배열을 완성하라
 		int pre = arr[0];
 		int answer_size = 1;
 		for (int value : arr) {
