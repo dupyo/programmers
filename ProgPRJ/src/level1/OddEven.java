4ed5cb471ea8805fcb46e29eb6711e562ee9cf19
dupyo <ghdrnvy0922@naver.com>
2021-07-26 17:34:35 +0900

    코드 정리. else 예약어 쓰지 않기

--- ProgPRJ/src/level1/OddEven.java
+++ ProgPRJ/src/level1/OddEven.java
@@ -3,15 +3,16 @@ package level1;
 public class OddEven {
 
 	public static void main(String[] args) {
-		//짝수와 홀수
+		// 짝수와 홀수
 		int num = 11289;
-		System.out.println(new OddEven().solution(num));
+		System.out.println(solution(num));
 
 	}
-	
-	//짝수와 홀수
-	public String solution(int num) {
-		return (num%2 == 0) ? "Even" : "Odd";
+
+	public static String solution(int num) {
+		if (num % 2 == 0)
+			return "Even";
+		return "Odd";
 	}
 
 }
