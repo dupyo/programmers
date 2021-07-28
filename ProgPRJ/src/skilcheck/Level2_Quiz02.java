3c3a0df19338d3947f6d892ba412565f43cc0c1f
dupyo <ghdrnvy0922@naver.com>
2021-07-28 13:21:51 +0900

    [전화번호 목록] 오름차순 정렬 후 접두어인지 확인

---
+++ ProgPRJ/src/skilcheck/Level2_Quiz02.java
@@ -0,0 +1,22 @@
+package skilcheck;
+
+import java.util.Arrays;
+
+public class Level2_Quiz02 {
+
+	public static void main(String[] args) {
+		// 전화번호 목록
+		String[] phone_book = { "119", "97674223", "1195524421" };
+		System.out.println(solution(phone_book));
+	}
+
+	public static boolean solution(String[] phone_book) {
+		Arrays.sort(phone_book);
+		for (int i = 0; i < phone_book.length - 1; i++) {
+			if (phone_book[i + 1].startsWith(phone_book[i]))
+				return false;
+		}
+		return true;
+	}
+
+}
