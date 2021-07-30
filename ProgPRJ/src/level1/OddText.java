034dab68ccd1d415cd911e9baf335693c4b086a9
dupyo <ghdrnvy0922@naver.com>
2021-07-30 23:33:39 +0900

    else 예약어 쓰지 않기

--- ProgPRJ/src/level1/OddText.java
+++ ProgPRJ/src/level1/OddText.java
@@ -18,10 +18,9 @@ public class OddText {
 			if (arr[i] == ' ') { // 공백 나오면 홀짝 구하는 인덱스 초기화
 				idx = 0;
 				continue;
-			} else if (idx % 2 == 1) {
-				arr[i] = (char) (arr[i] + 32); // 아스키 코드 대문자->소문자 변환
 			}
-			idx++;
+			if (idx++ % 2 == 1)
+				arr[i] = (char) (arr[i] + 32); // 아스키 코드 대문자->소문자 변환
 		}
 		return String.valueOf(arr); // char[] -> String
 	}
