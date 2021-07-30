791a814e129968bf08a1d68ade3e4c0067033de5
dupyo <ghdrnvy0922@naver.com>
2021-07-30 23:49:19 +0900

    코드 정리 필요함

--- ProgPRJ/src/level1/PnYInString.java
+++ ProgPRJ/src/level1/PnYInString.java
@@ -3,17 +3,18 @@ package level1;
 public class PnYInString {
 
 	public static void main(String[] args) {
-		//문자열 내 p와 y의 개수
-		String s="pPoooyY";
+		// 문자열 내 p와 y의 개수
+		String s = "pPoooyY";
 		System.out.println(new PnYInString().solution(s));
 	}
-	//문자열 내 p와 y의 개수
+
+	// 문자열 내 p와 y의 개수
 	public boolean solution(String s) {
-		int cnt=0;
-		s=s.toLowerCase();
-		//문자열에서 p의 개수와 y의 개수의 차를 이용
-		cnt=s.length()-s.replaceAll("p", "").length();
-		cnt-=s.length()-s.replaceAll("y", "").length();
+		int cnt = 0;
+		s = s.toLowerCase();
+		// 문자열에서 p의 개수와 y의 개수의 차를 이용
+		cnt = s.length() - s.replaceAll("p", "").length();
+		cnt -= s.length() - s.replaceAll("y", "").length();
 		return (cnt == 0);
 	}
 
