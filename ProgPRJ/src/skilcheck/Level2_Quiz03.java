c4617b2f8981a1cf3dcbb81d3e81a956d76be225
dupyo <ghdrnvy0922@naver.com>
2021-08-03 12:54:08 +0900

    [메뉴 리뉴얼] 문제 확인하고 풀어보자

---
+++ ProgPRJ/src/skilcheck/Level2_Quiz03.java
@@ -0,0 +1,17 @@
+package skilcheck;
+
+import java.util.Arrays;
+
+public class Level2_Quiz03 {
+
+	public static void main(String[] args) {
+		// 메뉴 리뉴얼
+		String[] menu = { "A", "B", "C" };
+		System.out.println(solution(menu));
+	}
+
+	public static boolean solution(String[] menu) {
+		return true;
+	}
+
+}
