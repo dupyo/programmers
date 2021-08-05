d9353b3161bf38a2cd42a19a174bd0122fc98771
dupyo <ghdrnvy0922@naver.com>
2021-08-05 11:18:37 +0900

    클래스 이름 명확하게 표현

--- ProgPRJ/src/level1/PositiveNegativeSum.java
+++ ProgPRJ/src/level1/PositiveNegativeSum.java
@@ -1,18 +1,18 @@
 package level1;
 
-public class PosiNegaSum {
+public class PositiveNegativeSum {
 
 	public static void main(String[] args) {
-		//음양 더하기
-		int[] absolutes = {4,7,12};
-		boolean[] signs = {true,false,true};
-		System.out.println(new PosiNegaSum().solution(absolutes, signs));
+		// 음양 더하기
+		int[] absolutes = { 4, 7, 12 };
+		boolean[] signs = { true, false, true };
+		System.out.println(solution(absolutes, signs));
 	}
 
-	public int solution(int[] absolutes, boolean[] signs) {
+	public static int solution(int[] absolutes, boolean[] signs) {
 		int answer = 0;
-		for(int i=0; i < absolutes.length; i++)
-			answer+=absolutes[i]*(signs[i] ? 1 : -1);
+		for (int i = 0; i < absolutes.length; i++)
+			answer += absolutes[i] * (signs[i] ? 1 : -1);
 		return answer;
 	}
 }
