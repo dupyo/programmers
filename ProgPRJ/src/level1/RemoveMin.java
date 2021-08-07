1d306854a961d57b0baeb983304afaf4dd1a8cf2
dupyo <ghdrnvy0922@naver.com>
2021-08-07 22:14:44 +0900

    정렬 없이 반복문으로 최솟값 구해서 연산 횟수 줄이기

--- ProgPRJ/src/level1/RemoveMin.java
+++ ProgPRJ/src/level1/RemoveMin.java
@@ -6,27 +6,24 @@ public class RemoveMin {
 
 	public static void main(String[] args) {
 		// 제일 작은 수 제거하기
-		int[] arr = { 10, 11, 4, 7, 3, 4 };
+		int[] arr = { 10, 11, 4, 7, 3 };
 		System.out.println(Arrays.toString(solution(arr)));
 	}
-
-	// 제일 작은 수 제거하기
 	public static int[] solution(int[] arr) {
 		int i = 0, j = 0;
 		if (arr.length == 1) {
 			arr[0] = -1;
 			return arr;
 		}
-		int[] ar2 = arr.clone();
-		Arrays.sort(ar2);
-		int min = ar2[0];
-
+		int min = arr[0];
+		for (int k = 1; k < arr.length; k++) {
+			if (arr[k] < min)
+				min = arr[k];
+		}
 		int[] answer = new int[arr.length - 1];
 		while (j < answer.length) {
-			if (min != arr[i]) {
-				answer[j] = arr[i];
-				j++;
-			}
+			if (min != arr[i])
+				answer[j++] = arr[i];
 			i++;
 		}
 		return answer;
