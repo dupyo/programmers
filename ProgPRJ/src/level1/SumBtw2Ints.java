926e2816c7039e524155486ec2f4e94eda80c10c
dupyo <ghdrnvy0922@naver.com>
2021-08-08 21:07:36 +0900

    코드 정리 필요함

--- ProgPRJ/src/level1/SumBtw2Ints.java
+++ ProgPRJ/src/level1/SumBtw2Ints.java
@@ -3,21 +3,22 @@ package level1;
 public class SumBtw2Ints {
 
 	public static void main(String[] args) {
-		int a=5;
-		int b=3;
-		System.out.println(new SumBtw2Ints().solution(a, b));
+		// 두 정수 사이의 합
+		int a = 5;
+		int b = 3;
+		System.out.println(solution(a, b));
 	}
-	
-	public long solution(int a, int b) {
+
+	public static long solution(int a, int b) {
 		long swNsum = 0;
-		if(a > b) {
-			swNsum=a;
-			a=b;
-			b=(int)swNsum;
-			swNsum=0;
+		if (a > b) {
+			swNsum = a;
+			a = b;
+			b = (int) swNsum;
+			swNsum = 0;
 		}
-		for(int i=a; i <= b; i++)
-			swNsum+=i;
+		for (int i = a; i <= b; i++)
+			swNsum += i;
 		return swNsum;
 	}
 
