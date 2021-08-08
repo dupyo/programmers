530ad97aba96f4952f9afce4ef1351ab8275cec2
dupyo <42468263+dupyo@users.noreply.github.com>
2021-08-08 16:19:07 +0900

    코드 정리 필요함

--- ProgPRJ/src/level1/ReverseTernary.java
+++ ProgPRJ/src/level1/ReverseTernary.java
@@ -3,21 +3,21 @@ package level1;
 public class ReverseTernary {
 
 	public static void main(String[] args) {
-		//3진법 뒤집기
+		// 3진법 뒤집기
 		int n = 125;
-		System.out.println(new ReverseTernary().solution(n));
+		System.out.println(solution(n));
 	}
 	
-	public int solution(int n) {
+	public static int solution(int n) {
 //		int answer = 0;
 		String ternaries = "";
 		while(n > 0) {
-			ternaries+=(n%3);
-//			ternaries=(n%3)+ternaries;
-			n=n/3;
+			ternaries += (n % 3);
+//			ternaries = (n % 3) + ternaries;
+			n = n / 3;
 		}
 		/*for(int i = 0; i < ternaries.length(); i++)
-			answer+=Math.pow(3, i)*(int)(ternaries.charAt(i)-'0');
+			answer += Math.pow(3, i) * (int)(ternaries.charAt(i) - '0');
 		return answer;*/
 		return Integer.parseInt(ternaries, 3);
 	}
