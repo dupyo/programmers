02d46cb7a80285ce85f656ebc6140408924e7d87
dupyo <ghdrnvy0922@naver.com>
2021-08-08 21:00:55 +0900

    진법 변환 메소드 parseInt 활용

--- ProgPRJ/src/level1/ReverseTernary.java
+++ ProgPRJ/src/level1/ReverseTernary.java
@@ -7,9 +7,10 @@ public class ReverseTernary {
 		int n = 125;
 		System.out.println(solution(n));
 	}
+
 	public static int solution(int n) {
 		String ternary = "";
-		while(n > 0) {
+		while (n > 0) {
 			ternary += (n % 3);
 			n /= 3;
 		}
