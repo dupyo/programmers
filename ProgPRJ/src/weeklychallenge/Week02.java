31b98ebbf1ce62922b0a94731b9c7903064752bc
dupyo <ghdrnvy0922@naver.com>
2021-08-12 16:59:10 +0900

    '상호 평가' 문제 해결

--- ProgPRJ/src/weeklychallenge/Week02.java
+++ ProgPRJ/src/weeklychallenge/Week02.java
@@ -8,38 +8,58 @@ public class Week02 {
 				{ 61, 57, 100, 80, 65 }, { 24, 90, 94, 75, 65 } };
 		System.out.println(solution(scores));
 	}
+
 	public static String solution(int[][] scores) {
 		// 1. 점수의 평균을 구한다.(자신이 부여한 점수가 유일한 최대 또는 최소라면 제외한다.)
 		// 2. 범위에 맞는 성적을 찾아 결과에 추가한다.(메소드 분리)
 		String answer = "";
-		int people = scores.length;
-		for(int i = 0; i < scores[0].length; i++) {
+		for (int i = 0; i < scores.length; i++) {
+			int people = scores.length;
+			int[] scorecount = new int[101]; // 0~100점까지 101개의 점수를 가리키는 배열
 			int sum = 0;
-			int myscore = scores[i][i];
-			int min = 100;
-			int max = 0;
-			for(int j = 0; j < scores.length; j++) {
-				min = Math.min(min, scores[j][i]);
-				max = Math.max(max, scores[j][i]);
-				if(myscore == min || myscore == max) {
-					sum += scores[j][i];
-					people--;
-				}
+			for (int j = 0; j < scores.length; j++) {
+				scorecount[scores[j][i]]++;
+				sum += scores[j][i];
 			}
-			System.out.println(min + ", " + max);
-			System.out.println(sum + ", " + people);
-			answer += credit(sum/people);
+			if (isMin(scores[i][i], scorecount) || isMax(scores[i][i], scorecount)) {
+				sum -= scores[i][i];
+				people--;
+			}
+			answer += credit(sum / people);
 		}
 		return answer;
 	}
-	public static String credit(int score) {
-		if(score >= 90)
+
+	public static boolean isMin(int myscore, int[] scorecount) {
+		for (int i = 0; i < scorecount.length; i++) {
+			if (scorecount[i] > 0) {
+				if (scorecount[i] == 1 && myscore == i)
+					return true;
+				return false;
+			}
+		}
+		return false;
+	}
+
+	public static boolean isMax(int myscore, int[] scorecount) {
+		for (int i = scorecount.length - 1; i >= 0; i--) {
+			if (scorecount[i] > 0) {
+				if (scorecount[i] == 1 && myscore == i)
+					return true;
+				return false;
+			}
+		}
+		return false;
+	}
+
+	public static String credit(int average) {
+		if (average >= 90)
 			return "A";
-		if(score >= 80)
+		if (average >= 80)
 			return "B";
-		if(score >= 70)
+		if (average >= 70)
 			return "C";
-		if(score >= 50)
+		if (average >= 50)
 			return "D";
 		return "F";
 	}
