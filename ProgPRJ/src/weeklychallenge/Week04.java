800fbd1761ab76d059c5f97233016da6e1053b92
dupyo <ghdrnvy0922@naver.com>
2021-08-24 21:00:16 +0900

    '직업군 추천하기' 문제 풀기

---
+++ ProgPRJ/src/weeklychallenge/Week04.java
@@ -0,0 +1,19 @@
+package weeklychallenge;
+
+public class Week04 {
+
+	public static void main(String[] args) {
+		// 직업군 추천하기
+		String[] table = { "SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA" };
+		String[] languages = { "PYTHON", "C++", "SQL" };
+		int[] preference = { 7, 5, 5 };
+		System.out.println(solution(table, languages, preference));
+	}
+
+	public static String solution(String[] table, String[] languages, int[] preference) {
+		// 1. 
+		String answer = "";
+		return answer;
+	}
+
+}
