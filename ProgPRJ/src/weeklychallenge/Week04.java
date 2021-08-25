9172dc51965b830699bd0ceeb6b5e666e6b0c2bd
dupyo <ghdrnvy0922@naver.com>
2021-08-25 22:35:12 +0900

    점수 부여하는 메소드 분리 및 예외 처리

--- ProgPRJ/src/weeklychallenge/Week04.java
+++ ProgPRJ/src/weeklychallenge/Week04.java
@@ -4,16 +4,35 @@ public class Week04 {
 
 	public static void main(String[] args) {
 		// 직업군 추천하기
-		String[] table = { "SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA" };
-		String[] languages = { "PYTHON", "C++", "SQL" };
-		int[] preference = { 7, 5, 5 };
+		String[] table = { "SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++",
+				"HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP",
+				"GAME C++ C# JAVASCRIPT C JAVA" };
+		String[] languages = { "JAVA", "JAVASCRIPT" };
+		int[] preference = { 7, 5 };
 		System.out.println(solution(table, languages, preference));
 	}
-
 	public static String solution(String[] table, String[] languages, int[] preference) {
-		// 1. 
 		String answer = "";
+		int max = 0;
+		for(int i = 0; i < table.length; i++) {
+			String[] rank = table[i].split(" ");
+			int sum = 0;
+			for(int j = 0; j < languages.length; j++) 
+				sum += (pointByRank(rank, languages[j]) * preference[j]);
+			
+			if(max <= sum) {
+				answer = (max == sum) ? (rank[0].compareTo(answer) < 0) ? rank[0] : answer : rank[0]; 
+				max = sum;
+			}
+		}
 		return answer;
+	}
+	public static int pointByRank(String[] rank, String language) {
+		for(int i = 1; i < rank.length; i++) {
+			if(rank[i].equals(language))
+				return 6 - i;
+		}
+		return 0;
 	}
 
 }
