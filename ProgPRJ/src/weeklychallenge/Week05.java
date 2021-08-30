249dc5d8b164062eabd37a59f01ba36ae0fec411
dupyo <ghdrnvy0922@naver.com>
2021-08-30 11:25:19 +0900

    [프로그래머스] 위클리 챌린지 5주차 풀기 '모음 사전'

---
+++ ProgPRJ/src/weeklychallenge/Week05.java
@@ -0,0 +1,16 @@
+package weeklychallenge;
+
+public class Week05 {
+
+	public static void main(String[] args) {
+		// 모음 사전
+		String word = "AAAAE";
+		System.out.println(solution(word));
+	}
+	public static int solution(String word) {
+		int answer = 0;
+		return answer;
+	}
+	
+
+}
