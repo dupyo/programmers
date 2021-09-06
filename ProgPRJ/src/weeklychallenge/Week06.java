1d915e38fdae6e9a29b9840be70f2485b7aa9e63
dupyo <ghdrnvy0922@naver.com>
2021-09-06 11:38:16 +0900

    [프로그래머스] 위클리 챌린지 6주차 문제 풀기 '복서 정렬하기'

---
+++ ProgPRJ/src/weeklychallenge/Week06.java
@@ -0,0 +1,17 @@
+package weeklychallenge;
+
+public class Week06 {
+
+	public static void main(String[] args) {
+		// 복서 정렬하기
+		int[] weights = { 50, 82, 75, 120 };
+		String[] head2head = { "NLWL", "WNLL", "LWNW", "WWLN" };
+		System.out.println(solution(weights, head2head));
+	}
+
+	public static int[] solution(int[] weights, String[] head2head) {
+		int[] answer = {};
+		return answer;
+	}
+
+}
