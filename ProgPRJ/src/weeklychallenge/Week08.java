f0a2c2e70c9caf88ea78599df91ad9f6be6e98ad
dupyo <ghdrnvy0922@naver.com>
2021-09-29 11:28:47 +0900

    [프로그래머스] 위클리 챌린지 8주차 문제 풀기 '최소직사각형'

---
+++ ProgPRJ/src/weeklychallenge/Week08.java
@@ -0,0 +1,18 @@
+package weeklychallenge;
+
+public class Week08 {
+
+	public static void main(String[] args) {
+		// 최소직사각형
+		int[][] size = { { 60, 50 }, { 30, 70 }, { 60, 30 }, { 80, 40 } };
+		System.out.println(solution(size)); // result : 4000
+	}
+
+	public static int solution(int[][] size) {
+		// 1. 가로 길이와 세로 길이에서 각각 최댓값을 구한다.
+		// 2. 각 명함의 크기에서 최댓값을 구한다.
+		int answer = 0;
+		return answer;
+	}
+
+}
