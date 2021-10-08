d8336255c6da8d1dae216acd03413276439ee050
dupyo <ghdrnvy0922@naver.com>
2021-10-08 12:57:00 +0900

    [프로그래머스] 위클리 챌린지 9주차 문제 풀기 '전력망을 둘로 나누기'

---
+++ ProgPRJ/src/weeklychallenge/Week09.java
@@ -0,0 +1,14 @@
+package weeklychallenge;
+
+public class Week09 {
+
+	public static void main(String[] args) {
+		// 전력망을 둘로 나누기
+	}
+
+	public static int solution(int n, int[][] wires) {
+		int answer = -1;
+		return answer;
+	}
+
+}
