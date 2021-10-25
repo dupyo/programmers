1f0b85dc436ee31eeec34fa97988d85169d053ae
dupyo <42468263+dupyo@users.noreply.github.com>
2021-10-25 22:36:24 +0900

    [프로그래머스] 위클리 챌린지 10주차 문제 풀기 '교점에 별 만들기'

---
+++ ProgPRJ/src/weeklychallenge/Week10.java
@@ -0,0 +1,17 @@
+package weeklychallenge;
+
+public class Week10 {
+
+	public static void main(String[] args) {
+		// 교점에 별 만들기
+		int[][] line = { { 2, -1, 4 }, { -2, -1, 4 }, { 0, -1, 1 }, { 5, -8, -12 }, { 5, 8, 12 } };
+		System.out.println(solution(line)); // result : { "....*....", ".........", ".........", "*.......*",
+											// ".........", ".........", ".........", ".........", "*.......*" }
+	}
+
+	public static String[] solution(int[][] line) {
+		String[] answer = {};
+		return answer;
+	}
+
+}
