198c76d59eb575c0a2c3b3fec9e6072bc5c833d4
dupyo <ghdrnvy0922@naver.com>
2021-11-01 19:12:51 +0900

    [프로그래머스] 위클리 챌린지 11주차 문제 풀기 '아이템 줍기'

---
+++ ProgPRJ/src/weeklychallenge/Week11.java
@@ -0,0 +1,20 @@
+package weeklychallenge;
+
+public class Week11 {
+
+	public static void main(String[] args) {
+		// 아이템 줍기
+		int[][] rectangle = { {1,1,7,4},{3,2,5,5},{4,3,6,9},{2,6,8,8} };
+		int characterX = 1;
+		int characterY = 3;
+		int itemX = 7;
+		int itemY = 8;
+		System.out.println(solution(rectangle, characterX, characterY, itemX, itemY)); // result : 17
+	}
+
+	public static int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+		int answer = 0;
+		return answer;
+	}
+
+}
