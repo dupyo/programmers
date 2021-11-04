26f42e8c20f44b0690150e002264d3a06d63418c
dupyo <42468263+dupyo@users.noreply.github.com>
2021-11-04 21:33:33 +0900

    [프로그래머스] 위클리 챌린지 12주차 문제 풀기 '피로도'

---
+++ ProgPRJ/src/weeklychallenge/Week12.java
@@ -0,0 +1,17 @@
+package weeklychallenge;
+
+public class Week12 {
+
+	public static void main(String[] args) {
+		// 피로도
+		int k = 80;
+		int[][] dungeons = { { 80, 20 }, { 50, 40 }, { 30, 10 } };
+		System.out.println(solution(k, dungeons)); // result : 3
+	}
+
+	public static int solution(int k, int[][] dungeons) {
+		int answer = -1;
+		return answer;
+	}
+
+}
