8e1e7e32db5dcb148a1f313a86a5ac357034ed28
dupyo <ghdrnvy0922@naver.com>
2021-11-16 17:26:14 +0900

    [Level2] '땅따먹기' 문제 풀기

---
+++ ProgPRJ/src/level2/Land.java
@@ -0,0 +1,16 @@
+package level2;
+
+public class Land {
+
+	public static void main(String[] args) {
+		// 땅따먹기
+		int[][] land = { { 1, 2, 3, 5 }, { 5, 6, 7, 8 }, { 4, 3, 2, 1 } };
+		System.out.println(solution(land));
+	}
+
+	public static int solution(int[][] land) {
+		int answer = 0;
+		return answer;
+	}
+
+}
