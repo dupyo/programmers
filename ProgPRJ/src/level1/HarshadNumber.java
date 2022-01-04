f416919f91b85e11e3ed9a74eaad933e8b7e6fd3
dupyo <ghdrnvy0922@naver.com>
2022-01-04 11:29:28 +0900

    [Level1] '하샤드 수' 문제 풀기2

--- ProgPRJ/src/level1/HarshadNumber.java
+++ ProgPRJ/src/level1/HarshadNumber.java
@@ -10,10 +10,15 @@ public class HarshadNumber {
 	
 	public static boolean solution(int x) {
 		int sum = 0;
-		int tmp = x;
-		while (tmp > 0) {
-			sum += tmp - (tmp / 10 * 10);
-			tmp /= 10;
+//		int tmp = x;
+//		while (tmp > 0) {
+//			sum += tmp - (tmp / 10 * 10);
+//			tmp /= 10;
+//		}
+		String[] str = String.valueOf(x).split(""); // 또는 split("0|")
+		for(String num : str) {
+			System.out.println(num);
+			sum += Integer.parseInt(num);
 		}
 		return x % sum == 0;
 	}
