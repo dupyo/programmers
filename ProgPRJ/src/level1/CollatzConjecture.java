83937c0c4babda7ae9bb8eda9f339b048bcdcd64
dupyo <ghdrnvy0922@naver.com>
2022-01-05 13:55:18 +0900

    [Level1] '콜라츠 추측' 문제 풀기2

--- ProgPRJ/src/level1/CollatzConjecture.java
+++ ProgPRJ/src/level1/CollatzConjecture.java
@@ -4,14 +4,13 @@ public class CollatzConjecture {
 
 	public static void main(String[] args) {
 		// 콜라츠 추측
-		int num = 4;
+		int num = 626331;
 		System.out.println(solution(num));
-
 	}
 	
 	public static int solution(int num) {
 		int cnt = 0;
-		while (num != 1 & num > 0 & cnt < 500) {
+		while (num > 1 & cnt < 500) { // 계산 과정에서 num이 정수형 범위를 벗어나면 음수로 변환되기 때문에 (num > 1) 조건식 사용
 			num = (num % 2 == 0) ? num / 2 : (num * 3) + 1;
 			cnt++;
 		}
