7e3f928a7f2957aba9e9e4d74124b0f2e31e3f82
dupyo <ghdrnvy0922@naver.com>
2022-01-06 13:25:56 +0900

    [Level1] '약수의 합' 문제 풀기2

--- ProgPRJ/src/level1/DivisorSum.java
+++ ProgPRJ/src/level1/DivisorSum.java
@@ -4,7 +4,7 @@ public class DivisorSum {
 
 	public static void main(String[] args) {
 		// 약수의 합
-		int n = 2;
+		int n = 25;
 		System.out.println(solution(n));
 	}
 
@@ -12,12 +12,10 @@ public class DivisorSum {
 		if (n == 1)
 			return n;
 		int sum = 0;
-		int tmp = n;
-		for (int i = 1; i < tmp; i++) {
-			if (n % i == 0) {
-				sum += (i * i == n) ? i : i + (n / i);
-				tmp = n / i;
-			}
+		double sqrt = Math.sqrt(n);
+		for (int i = 1; i <= sqrt; i++) {
+			if (n % i == 0)
+				sum += (i == sqrt) ? i : i + (n / i);
 		}
 		return sum;
 	}
