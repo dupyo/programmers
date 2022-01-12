caccaa79d81d29ba5fd56a85e60ba69be814a646
dupyo <ghdrnvy0922@naver.com>
2022-01-12 13:11:38 +0900

    [Level1] '문자열을 정수로 바꾸기' 문제 풀기2(풀이참고)

--- ProgPRJ/src/level1/String2Int.java
+++ ProgPRJ/src/level1/String2Int.java
@@ -4,13 +4,19 @@ public class String2Int {
 
 	public static void main(String[] args) {
 		// 문자열을 정수로 바꾸기
-		String s = "-1234";
+		String s = "-31";
 		System.out.println(solution(s));
 
 	}
 
 	public static int solution(String s) {
-		return Integer.parseInt(s);
+		// return Integer.parseInt(s);
+		int res = (s.charAt(0) - '0' > 0) ? s.charAt(0) - '0' : 0;
+		boolean sign = (s.charAt(0) == '-') ? false : true;
+		for (int i = 1; i < s.length(); i++) {
+			res = res * 10 + (s.charAt(i) - '0');
+		}
+		return sign ? res : -1 * res;
 	}
 
 }
