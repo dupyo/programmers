7c9e002df737e7e11ab5e2add047d536f41f58a6
dupyo <ghdrnvy0922@naver.com>
2022-01-18 16:08:59 +0900

    [Level1] '문자열 내 마음대로 정렬하기' 문제 풀기2

--- ProgPRJ/src/level1/OrderStrILike.java
+++ ProgPRJ/src/level1/OrderStrILike.java
@@ -7,31 +7,23 @@ public class OrderStrILike {
 
 	public static void main(String[] args) {
 		// 문자열 내 마음대로 정렬하기
-		String[] strings = {"abce", "abcd", "cdx"};
+		String[] strings = {"sun", "bed", "car"};
 		int n = 1;
 		System.out.println(Arrays.toString(solution(strings, n)));
 	}
 	
 	public static String[] solution(String[] strings, int n) {
-		String[] answer = new String [strings.length];
-		String[][] indexArr = new String[strings.length][2];
-		for (int i = 0; i < strings.length; i++) {
-			indexArr[i][0] = "" + strings[i].charAt(n);
-			indexArr[i][1] = strings[i];
-		}
-		Arrays.sort(indexArr, new Comparator<String[]>() {
-			public int compare(String[] arr1, String[] arr2) {
-				if (arr1[0].compareTo(arr2[0]) == 0)
-					return arr1[1].compareTo(arr2[1]);
-				if (arr1[0].compareTo(arr2[0]) > 0)
+		Arrays.sort(strings, new Comparator<String>() {
+			@Override
+			public int compare(String arr1, String arr2) { // 오름차순 정렬
+				if (arr1.charAt(n) == arr2.charAt(n))
+					return arr1.compareTo(arr2);
+				if (arr1.charAt(n) > arr2.charAt(n))
 					return 1;
 				return -1;
 			}
 		});
-		for (int i = 0; i < answer.length; i++) {
-			answer[i] = indexArr[i][1];
-		}
-		return answer;
+		return strings;
 	}
 
 }
