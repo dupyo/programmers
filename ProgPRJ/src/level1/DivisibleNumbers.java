43de1c6272921d009bf0fd279aca61cb23df6867
dupyo <ghdrnvy0922@naver.com>
2022-01-19 13:02:43 +0900

    [Level1] '나누어 떨어지는 숫자 배열' 문제 풀기2

--- ProgPRJ/src/level1/DivisibleNumbers.java
+++ ProgPRJ/src/level1/DivisibleNumbers.java
@@ -14,12 +14,11 @@ public class DivisibleNumbers {
 	public static int[] solution(int[] arr, int divisor) {
 		int cnt = 0;
 		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] % divisor == 0) {
+			if (arr[i] % divisor == 0)
 				arr[cnt++] = arr[i];
-			}
 		}
 		if (cnt == 0) {
-			int[] minusone = {-1};
+			int[] minusone = { -1 };
 			return minusone;
 		}
 		int[] answer = new int[cnt];
